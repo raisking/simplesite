6700d3be2721d477a08b34ab59f46fc9e80ee2ca
Prakash Rai <raiprakash@Prakashs-MBP.attlocal.net>
2021-04-04 14:20:46 -0400

    tst

--- src/Service.jsx
+++ src/Service.jsx
@@ -5,7 +5,7 @@ const Service = () => {
     return (
         <>
             <div className='my-5'>
-                <h1 className='text-center'>Our Services</h1>
+                <h1 className='text-center'>Ourr Services</h1>
             </div>
             <div className='container-fluid mb-5'>
                 <div className='row'>
